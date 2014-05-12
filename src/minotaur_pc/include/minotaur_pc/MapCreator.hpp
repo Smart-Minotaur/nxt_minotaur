0904035bba91ece5cf76046669685829adbed9f7
robolab <robolab@robolab-ESPRIMO-P910.(none)>
2014-05-12 18:46:00 +0200

    MapCreator

--- src/minotaur_pc/include/minotaur_pc/MapCreator.hpp
+++ src/minotaur_pc/include/minotaur_pc/MapCreator.hpp
@@ -2,6 +2,7 @@
 #define MAPCREATOR_HPP_
 
 #include "minotaur_pc/Map.hpp"
+#include "minotaur_pc/RobotPosition.hpp"
 
 #define RAD_TO_DEGREE 57.295779513082
 #define DEGREE_TO_RAD 0.017453292519943
@@ -12,14 +13,16 @@ namespace minotaur
   {
   private:
     Map map;
-    int grid[500][500];
   public:
     MapCreator();
+    MapCreator(int width, int height);
+    MapCreator(Map p_map);
     ~MapCreator();
     void setSensorDistances(int sensor, int x, int y);
     void step(int sensorValue1, int sensorValue2, int sensorValue3);
     void calculateObstaclePosition(int measuredDistance, const int sensor);
-    void setPosition(double x, double y, double currentDirection);
+    void setPosition(RobotPosition p_position);
+    Map getMap();
     void createTextFile();
   };
 }
