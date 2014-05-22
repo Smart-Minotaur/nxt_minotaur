15ce2679c3d12b36a61ee32c4e79e00812982124
Robotik-Labot <robolab@Robo13.(none)>
2014-05-22 11:25:43 +0200

    Fixed problems with variable names.

--- src/nxt_qt/include/nxt_qt/MouseMonitorWindow.hpp
+++ src/nxt_qt/include/nxt_qt/MouseMonitorWindow.hpp
@@ -8,42 +8,43 @@
 
 namespace minotaur
 {
-	class DirectionWidget : public QWidget
-	{
-	  private:
-	  MouseData data;
-	    
-	  public:
-	    MyWidget(QWidget *parent = 0):QWidget(parent) {}
-	    ~MyWidget() { }
-	    
-	    void updateWidget(MouseData data);
-	    
-	    void paintEvent(QPaintEvent *event);
-	};
-	
-	class MouseMonitorWindow : public QMainWindow, public Ui::MouseMonitorMainWindow
-	{
-		Q_OBJECT
 
-		private:
-			MouseMonitorNode monitorNode;
-			MyWidget *widget1;
-			MyWidget2 *widget2;
+    class DirectionWidget : public QWidget
+    {
+        private:
+            MouseData data;
 
-		private Q_SLOTS:
-			void processMouseData(const MouseData data);
+        public:
+            DirectionWidget(QWidget *parent = 0):QWidget(parent) {}
+            ~DirectionWidget() { }
 
-		public:
-			MouseMonitorWindow(QWidget *parent = 0);
-			virtual ~MouseMonitorWindow();
-		
-			MouseMonitorNode& getMonitorNode();
-			void paintEvent(QPaintEvent *event);
-	};
-	
-	
-	
+            void updateWidget(MouseData data);
+
+            void paintEvent(QPaintEvent *event);
+    };
+
+    class MouseMonitorWindow : public QMainWindow, public Ui::MouseMonitorMainWindow
+    {
+            Q_OBJECT
+
+        private:
+            MouseMonitorNode monitorNode;
+            DirectionWidget *widget1;
+            DirectionWidget *widget2;
+
+        private Q_SLOTS:
+            void processMouseData(const MouseData data);
+
+        public:
+            MouseMonitorWindow(QWidget *parent = 0);
+            virtual ~MouseMonitorWindow();
+
+            MouseMonitorNode& getMonitorNode();
+            void paintEvent(QPaintEvent *event);
+    };
+
+
+
 
 }
 
--- src/nxt_qt/src/MouseMonitorWindow.cpp
+++ src/nxt_qt/src/MouseMonitorWindow.cpp
@@ -16,98 +16,87 @@
 #define GRID_Y_MID ((GRID_Y_MAX - GRID_Y_START) / 2) + GRID_Y_START
 #define SCALE 20
 
-
-
 namespace minotaur
 {
 
-	MouseMonitorWindow::MouseMonitorWindow(QWidget *parent) :
-		QMainWindow(parent)
-	{
-		setupUi(this);
+    MouseMonitorWindow::MouseMonitorWindow(QWidget *parent) :
+        QMainWindow(parent)
+    {
+        setupUi(this);
 
-		widget1 = new DirectionWidget(widget); //widget = name of widget in ui
-		widget->layout()->addWidget(widget1);
-		
-		widget2 = new DirectionWidget(widget_2); //widget_2 = name of widget in ui
-		widget_2->layout()->addWidget(widget2);
-		
-		connect(&monitorNode, SIGNAL(measuredMouseData(const MouseData)),
-			this, SLOT(processMouseData(const MouseData)));
-	}
+        widget1 = new DirectionWidget(widget); // widget = name of widget in ui
+        widget1->layout()->addWidget(widget1);
 
-	MouseMonitorWindow::~MouseMonitorWindow() 
-	{
-	  delete widget1;
-	  delete widget2;
-	}
+        widget2 = new DirectionWidget(widget_2); //widget2 = name of widget in ui
+        widget2->layout()->addWidget(widget2);
 
-	MouseMonitorNode& MouseMonitorWindow::getMonitorNode()
-	{
-		return monitorNode;
-	}
+        connect(&monitorNode, SIGNAL(measuredMouseData(const MouseData)),
+                this, SLOT(processMouseData(const MouseData)));
+    }
 
-	
-	void MouseMonitorWindow::processMouseData(const MouseData data)
-	{
-		
-	  if (data.id == SENSOR1)
-	  {
-	    widget1->updateWidget(data);
-	  }
-	  else
-	  {
-	    widget2->updateWidget(data);
-	  }
-		
-	}
-	
-	
-	void MouseMonitorWindow::paintEvent(QPaintEvent *event)
-	{
-	  /*ROS_INFO("inside mouseMonitorWindow::paintevent");
-	  QPainter *painter = new QPainter(this);
-	  painter->begin(this);
-    
-	  // Add the vertical lines first, paint them
-	  painter->setPen(QPen(Qt::black, 1));
-	  for (int x = GRID_X_START; x <= GRID_X_MAX; x += SCALE)
-	    painter->drawLine(x, GRID_Y_START, x, GRID_Y_MAX);
+    MouseMonitorWindow::~MouseMonitorWindow()
+    {
+        delete widget1;
+        delete widget2;
+    }
 
-	  // Now add the horizontal lines, paint them
-	  for (int y = GRID_Y_START; y <= GRID_Y_MAX; y += SCALE)
-	    painter->drawLine(GRID_X_START, y, GRID_X_MAX, y);
-	  */
-	}
-	
-	
-	void DirectionWidget::paintEvent(QPaintEvent *event)
-	{
-	  QPainter *painter = new QPainter(this);
-	  painter->begin(this);
-    
-	  // Add the vertical lines first, paint them
-	  painter->setPen(QPen(Qt::black, 1));
-	  for (int x = GRID_X_START; x <= GRID_X_MAX; x += SCALE)
-	    painter->drawLine(x, GRID_Y_START, x, GRID_Y_MAX);
+    MouseMonitorNode& MouseMonitorWindow::getMonitorNode()
+    {
+        return monitorNode;
+    }
 
-	  // Now add the horizontal lines, paint them
-	  for (int y = GRID_Y_START; y <= GRID_Y_MAX; y += SCALE)
-	    painter->drawLine(GRID_X_START, y, GRID_X_MAX, y);
-	}
+    void MouseMonitorWindow::processMouseData(const MouseData data)
+    {
+        if (data.id == SENSOR1) {
+            widget1->updateWidget(data);
+        } else {
+            widget2->updateWidget(data);
+        }
+    }
 
-	
-	
-	void DirectionWidget::updateWidget(MouseData data)
-	{
-	  this->data = data;
-	  update();
-	}
-	
 
-	/*void MouseMonitorWindow::processMouseSettings(const )
-	{
+    void MouseMonitorWindow::paintEvent(QPaintEvent *event)
+    {
+        /*ROS_INFO("inside mouseMonitorWindow::paintevent");
+        QPainter *painter = new QPainter(this);
+        painter->begin(this);
 
-	}*/
+        // Add the vertical lines first, paint them
+        painter->setPen(QPen(Qt::black, 1));
+        for (int x = GRID_X_START; x <= GRID_X_MAX; x += SCALE)
+          painter->drawLine(x, GRID_Y_START, x, GRID_Y_MAX);
+
+        // Now add the horizontal lines, paint them
+        for (int y = GRID_Y_START; y <= GRID_Y_MAX; y += SCALE)
+          painter->drawLine(GRID_X_START, y, GRID_X_MAX, y);
+        */
+    }
+
+
+    void DirectionWidget::paintEvent(QPaintEvent *event)
+    {
+        QPainter *painter = new QPainter(this);
+        painter->begin(this);
+
+        // Add the vertical lines first, paint them
+        painter->setPen(QPen(Qt::black, 1));
+        for (int x = GRID_X_START; x <= GRID_X_MAX; x += SCALE)
+            painter->drawLine(x, GRID_Y_START, x, GRID_Y_MAX);
+
+        // Now add the horizontal lines, paint them
+        for (int y = GRID_Y_START; y <= GRID_Y_MAX; y += SCALE)
+            painter->drawLine(GRID_X_START, y, GRID_X_MAX, y);
+    }
+
+    void DirectionWidget::updateWidget(MouseData data)
+    {
+        this->data = data;
+        update();
+    }
+
+    /*void MouseMonitorWindow::processMouseSettings(const )
+    {
+
+    }*/
 
 }
