9d22e0712924d6da71287f74949c40ad7da4039d
Tobi <tobi@tobi-NB-linux.(none)>
2014-09-06 19:38:11 +0200

    deleted comments

--- src/mouse_monitor_pc/include/mouse_monitor_pc/MouseMonitorCalibrationWizard.hpp
+++ src/mouse_monitor_pc/include/mouse_monitor_pc/MouseMonitorCalibrationWizard.hpp
@@ -16,43 +16,6 @@ public:
 	MouseMonitorCalibrationWizard(QWidget * parent = 0);
 	~MouseMonitorCalibrationWizard() { };
 };
-/*
-class MouseMonitorCalibrationWizardDistancePage : public QWizardPage, public Ui::distancePage1
-{
-	Q_OBJECT
-	
-public: 
-	MouseMonitorCalibrationWizardDistancePage(QWidget * parent = 0);
-	~MouseMonitorCalibrationWizardDistancePage() { };
-private:
-
-};
-
-class MouseMonitorCalibrationWizardMeasurementPage : public QWizardPage, public Ui::measurePage2
-{
-	Q_OBJECT
-	
-	public:
-	MouseMonitorCalibrationWizardMeasurementPage(QWidget * parent = 0);
-	~MouseMonitorCalibrationWizardMeasurementPage() { };
-	
-	private:
-	float s1XEditValue;
-};
-
-class MouseMonitorCalibrationWizardResultPage : public QWizardPage, public Ui::resultPage3
-{
-	Q_OBJECT
-	
-	public:
-	MouseMonitorCalibrationWizardResultPage(QWidget * parent = 0);
-	~MouseMonitorCalibrationWizardResultPage() { };
-	
-	private:
-
-};
-*/
-
 
 }
 
--- src/mouse_monitor_pc/src/mouse_monitor_pc/MouseMonitorCalibrationWizard.cpp
+++ src/mouse_monitor_pc/src/mouse_monitor_pc/MouseMonitorCalibrationWizard.cpp
@@ -7,30 +7,7 @@ MouseMonitorCalibrationWizard::MouseMonitorCalibrationWizard(QWidget *parent)
      : QWizard(parent)
 {
 	setupUi(this);	
-	/*addPage(new MouseMonitorCalibrationWizardDistancePage);
-    addPage(new MouseMonitorCalibrationWizardMeasurementPage);
-    addPage(new MouseMonitorCalibrationWizardResultPage);
-	*/
 }
 
-/*
-MouseMonitorCalibrationWizardDistancePage::MouseMonitorCalibrationWizardDistancePage(QWidget *parent) 
-	: QWizardPage(parent)
-{
-}
-
-
-
-MouseMonitorCalibrationWizardMeasurementPage::MouseMonitorCalibrationWizardMeasurementPage(QWidget *parent)
-	: QWizardPage(parent)
-{
-}
-
-MouseMonitorCalibrationWizardResultPage::MouseMonitorCalibrationWizardResultPage(QWidget *parent)
-	: QWizardPage(parent)
-{
-}
-
-*/
 }
 
